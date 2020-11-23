1d877491632497f0f8d3582c9147f30cb50628ae
jpb <jean-pascal.boudet@atm-consulting.fr>
2020-11-23 12:33:40 +0100

    add interface to connector

---
+++ class/Connector.int.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+
+abstract class ConnectorInterface extends SeedObject {
+
+    /**
+     * ConnectorInterface constructor
+     *
+     */
+     public function __construct(&$db){
+
+         parent::__construct($db);
+
+     }
+
+    /**
+     * Function that need to be override by children
+     */
+     abstract public function setCredentials($accountCredentials);
+
+    /**
+     * Function that need to be override by children
+     */
+     abstract public function checkConnection();
+
+    /**
+     * Function that need to be override by children
+     */
+     abstract public function sendQuery($context,$params);
+
+     /**
+     * Function that need to be override by children
+     */
+     abstract function getCustomUri($context,$params);
+}
--- class/isiworkconnector.class.php
+++ class/isiworkconnector.class.php
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+require_once __DIR__.'/Connector.int.php';
 
 if (!class_exists('SeedObject'))
 {
@@ -25,7 +26,7 @@ if (!class_exists('SeedObject'))
 }
 
 
-class Isiworkconnector extends SeedObject
+class Isiworkconnector extends ConnectorInterface
 {
 
     public $Login;
