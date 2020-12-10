1dcf87d546e63b3eea0dd1a1e22cf20df9019701
kevin <kevin.giuga@atm-consulting.fr>
2020-12-10 17:43:54 +0100

    Refacto Class to ensure that it handle Upload and info fetch by itself

--- class/ZeenDoc.Connector.class.php
+++ class/ZeenDoc.Connector.class.php
@@ -19,7 +19,8 @@ require_once __DIR__.'/Connector.php';
 if (!class_exists('SeedObject'))
 {
 	/**
-	 * Needed if $form->showLinkedObjectBlock() is call or for session timeout on our module page
+	 * Needed if $form->showLinkedObjectBlock() is call or for session
+	timeout on our module page
 	 */
 	define('INC_FROM_DOLIBARR', true);
 	require_once dirname(__FILE__).'/../config.php';
@@ -30,25 +31,30 @@ class ZeenDocConnector extends Connector
 {
 
 	public $urlClient;
-	Public $baseUrl;
+	public $baseUrl;
 	public $lastXmlResult;
 	public $classeur;
 	public $idsource;
+
+	public $errors = "";
+	public $output = "";
+	public $fileUploaded = 0;
+	public $errorFileUpload = 0;
 
 	const  URI_EDIT_END_POINT = "Edit_Source.php?";
 	const  URI_PRE_UPLOAD_END_POINT = "Pre_Upload.php?";
 	const  URI_UPLOAD_END_POINT = "Upload.php?";
 	const  URI_POST_UPLOAD_END_POINT = "Post_Upload.php?";
 
-	const  ZEENDOC_STATUS_FILE_DELETED   = -1;
-	const  ZEENDOC_STATUS_FILE_INDEXED   = 1;
-	const  ZEENDOC_STATUS_FILE_TO_INDEX  = 2;
+	const  ZEENDOC_STATUS_FILE_DELETED = -1;
+	const  ZEENDOC_STATUS_FILE_INDEXED = 1;
+	const  ZEENDOC_STATUS_FILE_TO_INDEX = 2;
 	const  ZEENDOC_STATUS_FILE_PROTECTED = 3;
 
-	const  CONTEXT_SOURCE_ID  = 1;
-	const  CONTEXT_PRE_UPLOAD_VALIDATION  = 2;
-	const  CONTEXT_UPLOAD  = 3;
-	const  CONTEXT_POST_UPLOAD  = 4;
+	const  CONTEXT_SOURCE_ID = 1;
+	const  CONTEXT_PRE_UPLOAD_VALIDATION = 2;
+	const  CONTEXT_UPLOAD = 3;
+	const  CONTEXT_POST_UPLOAD = 4;
 
 
 	/**
@@ -56,7 +62,8 @@ class ZeenDocConnector extends Connector
 	 *
 	 * @param DoliDB $db
 	 */
-	function __construct(DoliDB &$db, $accountCredentials) {
+	function __construct(DoliDB &$db, $accountCredentials)
+	{
 
 		parent::__construct($db);
 		$this->setCredentials($accountCredentials);
@@ -67,90 +74,140 @@ class ZeenDocConnector extends Connector
 	 * @param string $password
 	 * @param string $urlClient
 	 * @param string $baseUrl
+	 * @param string $classeur
+	 * @param string $idsource
 	 */
-	public function setCredentials($accountCredentials){
+	public function setCredentials($accountCredentials)
+	{
 
-		$this->Login        = $accountCredentials->login;
-		$this->Password     = $accountCredentials->password;
-		$this->urlClient    = $accountCredentials->urlclient;
-		$this->baseUrl      = $accountCredentials->baseUrl;
-		$this->classeur     = $accountCredentials->classeur;
-		$this->idsource     = $accountCredentials->idsource;
+		$this->Login = $accountCredentials->login;
+		$this->Password = $accountCredentials->password;
+		$this->urlClient = $accountCredentials->urlclient;
+		$this->baseUrl = $accountCredentials->baseUrl;
+		$this->classeur = $accountCredentials->classeur;
+		$this->idsource = $accountCredentials->idsource;
 	}
-
 
 	/**
 	 * @return mixed
 	 */
-	public function getLastXmlErrorMsg(){
+	public function getLastXmlErrorMsg()
+	{
 
-		if (isset($this->lastXmlResult)){
-			return (string) $this->lastXmlResult->Error_Msg;
+		if (isset($this->lastXmlResult)) {
+			return (string)$this->lastXmlResult->Error_Msg;
 		}
 	}
 
+
 	/**
-	 * @param  int $context
-	 * @param  mixed $params
+	 * @param int   $context
+	 * @param mixed $params
 	 * @return int | SimpleXMLElement | string[]
 	 */
-	public function sendQuery($context,$params){
+	public function sendQuery($context, $params)
+	{
 
 		global $langs;
 		// Construit l'url correcte selon le contexte donné en paramètre
-		$baseUrl = $this->getCustomUri($context,$params);
-		// Dans le cas d'un upload,la fonction file_get_contents prend un param supplémentaire (le contextStream)
-		// On prépare celui-ci dans la fonction contextStreamForUpload
-		if($context == self::CONTEXT_UPLOAD){
-			$dataResult = file_get_contents($baseUrl, false, $this->contextStreamForUpload($params));
-		} else {
-			$dataResult = file_get_contents($baseUrl, false);
-		}
+		$baseUrl = $this->getCustomUri($context, $params);
 
+		$dataResult = file_get_contents($baseUrl, false);
 		// Transforme le $dataResult en un objet xml utilisable
 		$xml = simplexml_load_string($dataResult);
 		// Pour afficher les erreurs eventuelles depuis le cron
 		$this->lastXmlResult = $xml;
 
-		switch ($context) {
+		try {
 
-			case self::CONTEXT_PRE_UPLOAD_VALIDATION :
+			switch ($context) {
 
-				if (isset($xml->Result) && $xml->Result == 0) {
-					$result = ['success' => "ok", 'Upload_Id' => (string)$xml->Upload_Id];
-					return $result;
-				} else {
-					$result = ['success' => "ko", 'Error_Msg' => (string)$xml->Error_Msg];
-					return $result;
-				}
-				break;
+				case self::CONTEXT_SOURCE_ID :
+					if (isset($xml->Id_Source)) {
+						return (int)$xml->Id_Source;
+					} else {
+						return $xml->Result;
+					}
+					break;
 
-			case self::CONTEXT_UPLOAD :
+				case self::CONTEXT_PRE_UPLOAD_VALIDATION :
 
-				if (isset($xml->Result) && $xml->Result == 0) {
-					$result = ['success' => "ok"];
-					return $result;
-				} else {
-					$result = ['success' => "ko", 'Error_Msg' => (string)$xml->Error_Msg];
-					return $result;
-				}
-				break;
+					if (isset($xml->Result) && $xml->Result == 0) {
+						$result = ['success' => "ok", 'Upload_Id' =>
+							(string)$xml->Upload_Id];
+						return $result;
+					} else {
+						$result = ['success' => "ko", 'Error_Msg' =>
+							(string)$xml->Error_Msg];
+						return $result;
+					}
+					break;
 
-			case self::CONTEXT_POST_UPLOAD :
+				case self::CONTEXT_UPLOAD :
 
-				if (isset($xml->Result) && $xml->Result == 0) {
-					$result = ['success' => "ok"];
-					return $result;
-				} else {
-					$result = ['success' => "ko", 'Error_Msg' => (string)$xml->Error_Msg];
-					return $result;
-				}
-				break;
+					define('MULTIPART_BOUNDARY', '--------------------------' . microtime(true));
+
+					$header = 'Content-Type: multipart/form-data; boundary=' . MULTIPART_BOUNDARY;
+					// equivalent to <input type="file" name="uploaded_file"/>
+					define('FORM_FIELD', 'Upload_File');
+
+					$filename = $params['path'] . "/" . $params['fileName'];
+					$filepath = DOL_DATA_ROOT . "/" . $filename;
+					$file_content_to_upload = file_get_contents($filepath, true);
+
+					$ext = substr(strrchr($filename, '.'), 0);
+
+					$content = "--".MULTIPART_BOUNDARY."\r\n".
+						"Content-Disposition: form-data; name=\"".FORM_FIELD."\"; filename=\"".$params['upload_id'].$ext."\"\r\n".
+						"Content-Type: ".mime_content_type($filepath)."\r\n\r\n".
+						$file_content_to_upload."\r\n";
+
+					// signal end of request
+					$content .= "--".MULTIPART_BOUNDARY."--\r\n";
+
+					$contextStream = stream_context_create(array(
+						'http' => array(
+							'method'  => 'POST',
+							'header'  => $header,
+							'content' => $content,
+						)
+					));
+
+					$dataResultUpload = file_get_contents($baseUrl, false, $contextStream);
+					$xml = simplexml_load_string($dataResultUpload);
+
+					if (isset($xml->Result) && $xml->Result == 0) {
+						$result = ['success' => "ok"];
+						return $result;
+					} else {
+						$result = ['success' => "ko", 'Error_Msg' => (string)$xml->Error_Msg];
+						return $result;
+					}
+					// END case self::CONTEXT_UPLOAD
+					break;
+
+				case self::CONTEXT_POST_UPLOAD :
+
+					if (isset($xml->Result) && $xml->Result == 0) {
+						$result = ['success' => "ok"];
+						return $result;
+					} else {
+						$result = ['success' => "ko", 'Error_Msg' => (string)$xml->Error_Msg];
+						return $result;
+					}
+					// END case self::CONTEXT_POST_UPLOAD
+					break;
+			}
+
+			if ($dataResult === false) {
+				setEventMessage($langs->trans('ErrorApiCall'), "errors");
+				return 0;
+			}
+
+		} catch (Exception $e) {
+			trigger_error(sprintf('call Api failed with error #%d: %s', $e->getCode(), $e->getMessage()),E_USER_ERROR);
 		}
 
-		if ($dataResult === false) {
-			return 0;
-		}
 
 	}
 
@@ -159,24 +216,36 @@ class ZeenDocConnector extends Connector
 	 * @param mixed $params
 	 * @return string
 	 */
-	public function getCustomUri($context,$params){
+	public function getCustomUri($context, $params)
+	{
 		$baseUrl = "";
-		switch($context){
+		switch ($context) {
+			case self::CONTEXT_SOURCE_ID :
+
+				$baseUrl = $this->baseUrl . "/" . self::URI_EDIT_END_POINT;
+				$baseUrl .= 'Login=' . $this->Login
+					. '&CPassword=' . $this->Password
+					. '&Url_Client=' . $this->urlClient
+					. '&Coll_Id=' . $params['Coll_Id']
+					. '&Titre=' . $params['Titre']
+					. '&Id_Type_Source=5';
+
+				return $baseUrl;
 
 			case self::CONTEXT_PRE_UPLOAD_VALIDATION :
 
-				$baseUrl = $this->baseUrl ."/". self::URI_PRE_UPLOAD_END_POINT;
-				$baseUrl.= 'Login='.$this->Login
-					.'&CPassword='.$this->Password
-					.'&Url_Client='.$this->urlClient
-					.'&Coll_Id='.$params['Coll_Id']
-					.'&FileName='.$params['fileName']
-					.'&MD5='.md5_file(DOL_DATA_ROOT."/".$params['path']."/".urldecode($params['fileName']))
-					.'&Id_Source='.$params['sourceId'];
+				$baseUrl = $this->baseUrl . "/" . self::URI_PRE_UPLOAD_END_POINT;
+				$baseUrl .= 'Login=' . $this->Login
+					. '&CPassword=' . $this->Password
+					. '&Url_Client=' . $this->urlClient
+					. '&Coll_Id=' . $params['Coll_Id']
+					. '&FileName=' . urlencode($params['fileName'])
+					. '&MD5=' . md5_file(DOL_DATA_ROOT . "/" .$params['path'] . "/" . $params['fileName'])
+					. '&Id_Source=' . $params['sourceId'];
 
-				if(!empty($params['CustomClassement'])) {
+				if (!empty($params['CustomClassement'])) {
 					foreach ($params['CustomClassement'] as $key => $custom) {
-						$baseUrl .= '&'.$key.'='.$custom;
+						$baseUrl .= '&' . $key . '=' . $custom;
 					}
 				}
 
@@ -184,66 +253,155 @@ class ZeenDocConnector extends Connector
 
 			case self::CONTEXT_UPLOAD :
 
-				$baseUrl = $this->baseUrl ."/". self::URI_UPLOAD_END_POINT;
-				$baseUrl.= 'Login='.$this->Login
-					.'&CPassword='.$this->Password
-					.'&Url_Client='.$this->urlClient
-					.'&Coll_Id='.$params['Coll_Id'];
+				$baseUrl = $this->baseUrl . "/" . self::URI_UPLOAD_END_POINT;
+				$baseUrl .= 'Login=' . $this->Login
+					. '&CPassword=' . $this->Password
+					. '&Url_Client=' . $this->urlClient
+					. '&Coll_Id=' . $params['Coll_Id'];
 
 				return $baseUrl;
 
 
 			case self::CONTEXT_POST_UPLOAD :
 
-				$baseUrl = $this->baseUrl ."/". self::URI_POST_UPLOAD_END_POINT;
-				$baseUrl.= 'Login='.$this->Login
-					.'&CPassword='.$this->Password
-					.'&Url_Client='.$this->urlClient
-					.'&Coll_Id='.$params['Coll_Id']
-					.'&Upload_Id='.$params['upload_id'];
+				$baseUrl = $this->baseUrl . "/" . self::URI_POST_UPLOAD_END_POINT;
+				$baseUrl .= 'Login=' . $this->Login
+					. '&CPassword=' . $this->Password
+					. '&Url_Client=' . $this->urlClient
+					. '&Coll_Id=' . $params['Coll_Id']
+					. '&Upload_Id=' . $params['upload_id'];
 
 				return $baseUrl;
 		}
 
 	}
 
+
 	/**
-	 * To prepare the file that will be uploaded
-	 * @param mixed $params
-	 * @return resource $contextStream
+	 * @param string $alreadySentFile
+	 * @return array|bool|mixed|object
+	 * @throws SoapFault
 	 */
-	public function contextStreamForUpload($params){
+	public function fetchfile($alreadySentFile)
+	{
+		global $langs;
+		/**
+		 * Soap Call Preparation
+		 */
+		$wsdl = "https://armoires.zeendoc.com/" . $this->urlClient . "/ws/1_0/wsdl.php?WSDL";
 
-		define('MULTIPART_BOUNDARY', '--------------------------'.microtime(true));
+		$client = new SoapClient($wsdl);
 
-		$header = 'Content-Type: multipart/form-data; boundary='.MULTIPART_BOUNDARY;
-		// equivalent to <input type="file" name="uploaded_file"/>
-		define('FORM_FIELD', 'Upload_File');
-
-		$filename = $params['path']."/".urldecode($params['fileName']);
-		$filepath = DOL_DATA_ROOT."/".$filename;
-		$file_content_to_upload = file_get_contents($filepath, true);
-
-		$ext = substr(strrchr($filename, '.'), 0);
-
-		$content =  "--".MULTIPART_BOUNDARY."\r\n".
-			"Content-Disposition: form-data; name=\"".FORM_FIELD."\"; filename=\"".$params['upload_id'].$ext."\"\r\n".
-			"Content-Type: ".mime_content_type($filepath)."\r\n\r\n".
-			$file_content_to_upload."\r\n";
-
-		// signal end of request
-		$content .= "--".MULTIPART_BOUNDARY."--\r\n";
-
-		$contextStream = stream_context_create(array(
-			'http' => array(
-				'method' => 'POST',
-				'header' => $header,
-				'content' => $content,
+		// On transforme le retour SOAP en objet
+		$rights = json_decode($client->__soapCall("getRights",
+			array(
+				new SoapParam($this->Login, 'Login'),
+				new SoapParam('', 'Password'),
+				new SoapParam($this->Password, 'CPassword'),
 			)
 		));
 
-		return $contextStream;
+		// Si nous avons une Connexion
+		if ($rights->Result == 0) {
+
+			$ext = substr(strrchr($alreadySentFile->filename, '.'), 0);
+			$complextTypeIndex1 = [
+				"Id"       => 1,
+				"Label"    => 'Filename',
+				"Value"    => basename($alreadySentFile->filename, $ext),
+				"Operator" => 'EQUALS',
+			];
+
+			$complextTypeIndex2 = [
+				"Id"       => 2,
+				"Label"    => 'Upload_Id',
+				"Value"    =>  $alreadySentFile->upload_id_zeendoc,
+				"Operator" => 'EQUALS',
+			];
+
+			$IndexListAsSoapVarObject = new SoapVar
+			(
+				array(
+					new SoapVar($complextTypeIndex1, SOAP_ENC_OBJECT,'IndexDefinition', null, 'Index'),
+					new SoapVar($complextTypeIndex2, SOAP_ENC_OBJECT,'IndexDefinition', null, 'Index'),
+				)
+				,
+				SOAP_ENC_OBJECT,
+				'ArrayOfIndexDefinition',
+				null,
+				'IndexList'
+			);
+
+			// Récupération des infos sur un document stocké chez ZeenDoc
+			$searchDoc = json_decode($client->__soapCall("searchDoc",
+				array(
+					new SoapParam($this->Login, 'Login'),
+					new SoapParam('', 'Password'),
+					new SoapParam($this->Password, 'CPassword'),
+					new SoapParam($this->classeur, 'Coll_Id'),
+					$IndexListAsSoapVarObject,
+					new SoapParam(0, 'StrictMode'),
+					new SoapParam('', 'Fuzzy'),
+					new SoapParam('', 'Order_Col'),
+					new SoapParam('', 'Order'),
+					new SoapParam('', 'saved_query'),
+					new SoapParam('', 'Query_Operator'),
+				)
+			));
+
+			return $searchDoc;
+
+		} else{
+			$this->output .= $langs->trans("WrongCredentialsZeendoc");
+			$this->errors++;
+		}
 	}
+
+
+	/**
+	 * @param string $fileToSend
+	 * @return mixed|SimpleXMLElement|string
+	 */
+	public function send($fileToSend) {
+
+		$params = [
+			'fileName'   => $fileToSend->filename
+			, 'path'     => $fileToSend->filepath
+			, 'sourceId' => $this->idsource
+			, 'Coll_Id'  => $this->classeur
+			, 'Titre'    => 'Nomsource'
+		];
+
+		$result = $this->sendQuery(self::CONTEXT_PRE_UPLOAD_VALIDATION, $params);
+
+		if ($result['success'] == "ok") {
+
+			// Upload du fichier
+			$params['upload_id'] = $result['Upload_Id'];
+			$resultUpload = $this->sendQuery(self::CONTEXT_UPLOAD, $params);
+
+			if ($resultUpload['success'] == "ok") {
+				// Check post-upload
+				$resultPostUpload = $this->sendQuery(self::CONTEXT_POST_UPLOAD, $params);
+
+				if ($resultPostUpload['success'] == "ok"){
+					$this->fileUploaded++;
+					return $result['Upload_Id'];
+				} else {
+					$this->errorFileUpload++;
+				}
+			} else {
+				// errors upload
+				$this->output .= $this->getLastXmlErrorMsg();
+				$this->errors++;
+			}
+		} else {
+			// errors pre-upload
+			$this->output = $this->getLastXmlErrorMsg();
+			$this->errors++;
+		}
+	}
+
 
 
 
--- core/modules/modisiworkconnector.class.php
+++ core/modules/modisiworkconnector.class.php
@@ -44,7 +44,7 @@ class modisiworkconnector extends DolibarrModules
 
 		$this->editor_name = 'ATM-Consulting';
 		$this->editor_url = 'https://www.atm-consulting.fr';
-		
+
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
 		$this->numero = 104654; // 104000 to 104999 for ATM CONSULTING
@@ -59,7 +59,7 @@ class modisiworkconnector extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Description of module isiworkconnector";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.0.0';
+		$this->version = '1.1.0';
 		// Key used in llx_const table to save module status enabled/disabled (where ISIWORKCONNECTOR is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
@@ -68,7 +68,7 @@ class modisiworkconnector extends DolibarrModules
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
 		$this->picto='isiworkconnector@isiworkconnector';
-		
+
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /isiworkconnector/core/xxxxx) (0=disable, 1=enable)
 		// for specific path of parts (eg: /isiworkconnector/core/modules/barcode)
@@ -187,7 +187,7 @@ class modisiworkconnector extends DolibarrModules
 		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$this->rights[$r][5] = '';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
-		
+
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'isiworkconnector_write';	// Permission label
 		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
@@ -238,7 +238,7 @@ class modisiworkconnector extends DolibarrModules
 		//							'target'=>'',
 		//							'user'=>2);				                // 0=Menu for internal users, 1=external users, 2=both
 		// $r++;
-		
+
 /*
 		$this->menu[$r]=array(
 			'fk_menu'=>0,			                // Put 0 if this is a top menu
@@ -305,7 +305,7 @@ class modisiworkconnector extends DolibarrModules
 		);				                // 0=Menu for internal users, 1=external users, 2=both
 		$r++;
 */
-		
+
 		// Exports
 		$r=1;
 
@@ -335,7 +335,7 @@ class modisiworkconnector extends DolibarrModules
 	public function init($options = '')
 	{
 		$sql = array();
-		
+
 		define('INC_FROM_DOLIBARR', true);
 
 		require dol_buildpath('/isiworkconnector/script/create-maj-base.php');
