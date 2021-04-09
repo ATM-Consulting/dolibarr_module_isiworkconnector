204235fc502fca1b9fbcbfe75981123c9b8484f1
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2021-04-09 14:08:12 +0200

    FIX : Remove useless conf in setup

--- admin/isiworkconnector_setup.php
+++ admin/isiworkconnector_setup.php
@@ -16,10 +16,10 @@
  */
 
 /**
- * 	\file		admin/isiworkconnector.php
- * 	\ingroup	isiworkconnector
- * 	\brief		This file is an example module setup page
- * 				Put some comments here
+ *    \file        admin/isiworkconnector.php
+ *    \ingroup    isiworkconnector
+ *    \brief        This file is an example module setup page
+ *                Put some comments here
  */
 // Dolibarr environment
 $res = @include '../../main.inc.php'; // From htdocs directory
@@ -28,7 +28,7 @@ if (! $res) {
 }
 
 // Libraries
-require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 require_once '../lib/isiworkconnector.lib.php';
 dol_include_once('abricot/includes/lib/admin.lib.php');
 
@@ -36,9 +36,7 @@ dol_include_once('abricot/includes/lib/admin.lib.php');
 $langs->loadLangs(array('isiworkconnector@isiworkconnector', 'admin', 'other'));
 
 // Access control
-if (! $user->admin) {
-    accessforbidden();
-}
+if(! $user->admin) accessforbidden();
 
 // Parameters
 $action = GETPOST('action', 'alpha');
@@ -46,43 +44,36 @@ $action = GETPOST('action', 'alpha');
 /*
  * Actions
  */
-if (preg_match('/set_(.*)/', $action, $reg))
-{
-	$code=$reg[1];
-	if (dolibarr_set_const($db, $code, GETPOST($code), 'chaine', 0, '', $conf->entity) > 0)
-	{
-		header("Location: ".$_SERVER["PHP_SELF"]);
-		exit;
-	}
-	else
-	{
-		dol_print_error($db);
-	}
+if(preg_match('/set_(.*)/', $action, $reg)) {
+    $code = $reg[1];
+    if(dolibarr_set_const($db, $code, GETPOST($code), 'chaine', 0, '', $conf->entity) > 0) {
+        header('Location: '.$_SERVER['PHP_SELF']);
+        exit;
+    }
+    else {
+        dol_print_error($db);
+    }
 }
 
-if (preg_match('/del_(.*)/', $action, $reg))
-{
-	$code=$reg[1];
-	if (dolibarr_del_const($db, $code, 0) > 0)
-	{
-		Header("Location: ".$_SERVER["PHP_SELF"]);
-		exit;
-	}
-	else
-	{
-		dol_print_error($db);
-	}
+if(preg_match('/del_(.*)/', $action, $reg)) {
+    $code = $reg[1];
+    if(dolibarr_del_const($db, $code, 0) > 0) {
+        header('Location: '.$_SERVER['PHP_SELF']);
+        exit;
+    }
+    else {
+        dol_print_error($db);
+    }
 }
 
 /*
  * View
  */
-$page_name = "isiworkconnectorSetup";
+$page_name = 'isiworkconnectorSetup';
 llxHeader('', $langs->trans($page_name));
 
 // Subheader
-$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">'
-    . $langs->trans("BackToModuleList") . '</a>';
+$linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans('BackToModuleList').'</a>';
 print load_fiche_titre($langs->trans($page_name), $linkback);
 
 // Configuration header
@@ -96,59 +87,38 @@ dol_fiche_head(
 );
 
 // Setup page goes here
-$form=new Form($db);
-$var=false;
-print '<table class="noborder" width="100%">';
+$form = new Form($db);
+$var = false;
 
+//-------------------------------------------------------------------------------
+
+print load_fiche_titre('ZEENDOC', '', '');
+print '<table class="noborder" width="100%">';
 
 if(!function_exists('setup_print_title')){
     print '<div class="error" >'.$langs->trans('AbricotNeedUpdate').' : <a href="http://wiki.atm-consulting.fr/index.php/Accueil#Abricot" target="_blank"><i class="fa fa-info"></i> Wiki</a></div>';
     exit;
 }
 
-setup_print_title("Parameters");
-
-// Example with a yes / no select
-setup_print_on_off('CONSTNAME', $langs->trans('ParamLabel'), 'ParamDesc');
-
-// Example with imput
-setup_print_input_form_part('CONSTNAME', $langs->trans('ParamLabel'));
-
-// Example with color
-setup_print_input_form_part('CONSTNAME', $langs->trans('ParamLabel'), 'ParamDesc', array('type'=>'color'), 'input', 'ParamHelp');
-
-// Example with placeholder
-//setup_print_input_form_part('CONSTNAME',$langs->trans('ParamLabel'),'ParamDesc',array('placeholder'=>'http://'),'input','ParamHelp');
-
-// Example with textarea
-//setup_print_input_form_part('CONSTNAME',$langs->trans('ParamLabel'),'ParamDesc',array(),'textarea');
-
-print '</table>';
-
-
-//-------------------------------------------------------------------------------
-print '<br />';
-print load_fiche_titre('ZEENDOC', '', '');
-print '<table class="noborder" width="100%">';
 setup_print_title($langs->trans('ZeenDocSendParameters'));
 
 $params = ['type'=>'text', 'placeholder' => 'login'];
-setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_LOGIN', $langs->trans('FINANCEMENT_URL_ZEENDOC_LOGIN'), '', $params, 'input');
+setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_LOGIN', $langs->trans('FINANCEMENT_URL_ZEENDOC_LOGIN'), '', $params);
 
 $params = ['type'=>'password', 'placeholder' => '*******'];
-setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_PASSWORD', $langs->trans('FINANCEMENT_URL_ZEENDOC_PASSWORD'), '', $params, 'input');
+setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_PASSWORD', $langs->trans('FINANCEMENT_URL_ZEENDOC_PASSWORD'), '', $params);
 
 $params = ['type'=>'text', 'placeholder' => ''];
-setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_URLCLIENT', $langs->trans('FINANCEMENT_URL_ZEENDOC_URLCLIENT'), '', $params, 'input');
+setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_URLCLIENT', $langs->trans('FINANCEMENT_URL_ZEENDOC_URLCLIENT'), '', $params);
 
 $params = ['type'=>'url', 'placeholder' => 'https://armoires.Zeendoc.com/_WebServices/Upload/0_3'];
-setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_SERVICE_URL_BASE', $langs->trans('FINANCEMENT_URL_ZEENDOC_SERVICE_URL_BASE'), '', $params, 'input');
+setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_SERVICE_URL_BASE', $langs->trans('FINANCEMENT_URL_ZEENDOC_SERVICE_URL_BASE'), '', $params);
 
 $params = ['type'=>'text', 'placeholder' => "nom du classeur"];
-setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_CLASSEUR', $langs->trans('FINANCEMENT_URL_ZEENDOC_CLASSEUR'), '', $params, 'input');
+setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_CLASSEUR', $langs->trans('FINANCEMENT_URL_ZEENDOC_CLASSEUR'), '', $params);
 
 $params = ['type'=>'text', 'placeholder' => "Source de documents"];
-setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_IDSOURCE', $langs->trans('FINANCEMENT_URL_ZEENDOC_IDSOURCE'), '', $params, 'input');
+setup_print_input_form_part('FINANCEMENT_URL_ZEENDOC_IDSOURCE', $langs->trans('FINANCEMENT_URL_ZEENDOC_IDSOURCE'), '', $params);
 
 print '</table>';
 
